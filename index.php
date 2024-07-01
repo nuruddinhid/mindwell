f87bf196606c093dc42a66591409cd8739ed02a4
Nuruddin Hidayat <nuruddinh065@gmail.com>
2024-07-01 18:47:49 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mindwell Clinic</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            <div class="navbar-brand">
+                Mindwell Clinic
+            </div>
+            <nav>
+                <a href="#home">Beranda</a>
+                <a href="#about">Tentang</a>
+                <a href="#services">Layanan</a>
+                <a href="#contact">Kontak</a>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <h2>Selamat Datang di Mindwell Clinic!</h2>
+        <p>"MindWell Clinic" adalah praktik psikologi yang menawarkan berbagai layanan konsultasi kejiwaan yang komprehensif. Klinik ini berkomitmen untuk memberikan dukungan dan perawatan yang berkualitas tinggi kepada individu yang membutuhkan bantuan dengan masalah kesehatan mental. Layanan yang tersedia di MindWell Clinic mencakup terapi individu, terapi kelompok, konseling keluarga, serta penilaian dan diagnosis kondisi kejiwaan. Dengan pendekatan yang berfokus pada kesejahteraan holistik, MindWell Clinic berusaha membantu klien mencapai kesehatan mental yang optimal dan meningkatkan kualitas hidup mereka.</p>
+    </main>
+    <footer>
+        <p>&copy; 2024 Mindwell Clinic</p>
+    </footer>
+</body>
+</html>
